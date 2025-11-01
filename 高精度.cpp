2ed04a191e5fc470f53b079a7ab17da069fe68da
Trannnnnnnnnnn <lz3393350798@gmail.com>
2025-11-01 15:16:02 +0800

    Refactor high precision algorithm by removing main function
    Removed the main function and related logic for factorial sum calculation.

--- 高精度.cpp
+++ 高精度.cpp
@@ -1,6 +1,5 @@
-/*典型的高精度算法，使用stl容器存储，相比使用数组效率更高，主函数求阶乘之和*/
-#include <bits/stdc++.h>
-using namespace std;
+/*典型的高精度算法，使用stl容器存储，相比使用数组效率更高*/
+//高精加
 vector<int> add(vector<int> &a,vector<int> &b){
     vector<int> c;
     int t=0;
@@ -13,6 +12,7 @@ vector<int> add(vector<int> &a,vector<int> &b){
     if(t) c.push_back(t);
     return c;
 }
+//高精乘
 vector<int> mul(vector<int> &a,int b){
     vector<int> c;
     int t=0;
@@ -24,22 +24,10 @@ vector<int> mul(vector<int> &a,int b){
     while(c.size()>1&&c.back()==0) c.pop_back();
     return c;
 }
+//高精打印
 void print(vector<int> &a){
     for(int i=a.size()-1;i>=0;i--){
         cout<<a[i];
     }
     cout << endl;
-}
-int main(){
-    int n;
-    cin>>n;
-    vector<int> sum,s;
-    sum.push_back(1);
-    s.push_back(0);
-    for(int i=1;i<=n;i++){
-        sum=mul(sum,i);
-        s=add(s,sum);
-    }
-    print(s);
-    return 0;
 }
